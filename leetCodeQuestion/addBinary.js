9f4052061f797fd0d3ca3d1d54debe32bb3217ce
Aditya Singh <65831065+sadityaa73@users.noreply.github.com>
2023-05-27 17:23:05 +0530

    modified and tested '

--- leetCodeQuestion/addBinary.js
+++ leetCodeQuestion/addBinary.js
@@ -6,75 +6,15 @@
 // Input: a = "1010", b = "1011"
 // Output: "10101"
 
-let a = "1010";
-let b = "1011";
+let a = "0";
+let b = "0";
 
 function addBinary(string1, string2) {
-  let sum = 0;
-  let carry = 0;
-  let result = "";
   let final = "";
-  let i = 0;
-  let j = 0;
-
-  let a = "";
-  let b = "";
-
-  for (let count1 = string1.length-1; count1 >= 0; count1--) {
-    a += string1[count1];
-  }
-  for (let count2 = string2.length-1; count2 >= 0; count2--) {
-    b += string2[count2];
-  }
- console.log("a",a,"b",b);
-  while (i < a.length && j < b.length) {
-    console.log("first while called");
-    sum = Number(a[i]) + Number(b[j]) + carry;
-    if (sum >= 2) {
-      carry = 1;
-      sum = 0;
-    } else if (sum < 2 && sum > 0) {
-      carry = 0;
-      sum = 1;
-    }
-    result += sum.toString();
-    i++;
-    j++;
-  }
-  while (i < a.length) {
-    console.log(" second while called");
-    sum = Number(a[i]) + carry;
-    if (sum >= 2) {
-      sum = 0;
-      carry = 1;
-    } else if (sum < 2 && sum > 0) {
-      sum = 1;
-      carry = 0;
-    }
-    result += sum.toString();
-    i++;
-  }
-  while (j < b.length) {
-    console.log(" third while called");
-    sum = Number(b[j]) + carry;
-    if (sum >= 2) {
-      sum = 0;
-      carry = 1;
-    } else if (sum < 2 && sum >= 0) {
-      sum = 1;
-      carry = 0;
-    }
-    result += sum.toString();
-    j++;
-  }
-  if (carry) {
-    console.log("if seciton called");
-    sum = carry;
-    result += sum.toString();
-  }
-  for (let k = result.length - 1; k >= 0; k--) {
-    final += result[k];
-  }
+  // we used BigInt() to convert binary to Integer. Syntax is BigInt(0b11) here `0b` is prefix and `11` is binary number;
+  final = BigInt(`0b${string1}`) + BigInt(`0b${string2}`);
+  // sum is converted back to string using toString(2), here 2 is radix argument, so toString(2) converts to binary;
+  final = final.toString(2);
   return final;
 }
 
