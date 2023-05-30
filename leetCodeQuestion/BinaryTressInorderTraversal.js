cfa73994de426447529df26f49cb900e33d1fa71
Aditya Singh <65831065+sadityaa73@users.noreply.github.com>
2023-05-30 18:10:25 +0530

    binary tree inorder traversal complted bet not tested some improvement required

---
+++ leetCodeQuestion/BinaryTressInorderTraversal.js
@@ -0,0 +1,42 @@
+//WAP To Binary Tree Inorder Traversal;
+//Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+// Input: root = [1,null,2,3]
+// Output: [1,3,2]
+
+
+//creating a node ;
+
+class Node{
+    constructor(data)
+    {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+};
+
+var root =null;
+function printInoredrTraversal(node)
+{
+    if(node === null)
+    {
+        return;
+    }
+  
+    printInoredrTraversal(node.left);
+    console.log(node.data + " ");
+    printInoredrTraversal(node.right);
+   return ;
+}
+
+
+function implementingTree()
+{
+    let root = new Node(1);
+    root.left= new Node(2);
+    root.right = new Node(3);
+    printInoredrTraversal(root);
+}
+
+implementingTree();
