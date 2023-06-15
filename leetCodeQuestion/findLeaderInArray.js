3111abb89c80d81f70337c41131640e85e98e358
Aditya Singh <65831065+sadityaa73@users.noreply.github.com>
2023-06-16 01:00:54 +0530

    find leader in array partially commit

---
+++ leetCodeQuestion/findLeaderInArray.js
@@ -0,0 +1,27 @@
+//WAP To Find Leader In Array:
+// Given an array A of positive integers. 
+// Your task is to find the leaders in the array.
+// An element of array is leader if it is greater than or
+// equal to all the elements to its right side. 
+// The rightmost element is always a leader. 
+
+//Example:
+// Input:
+// n = 6
+// A[] = {16,17,4,3,5,2}
+// Output: 17 5 2
+// Explanation: The first leader is 17 
+// as it is greater than all the elements
+// to its right.  Similarly, the next 
+// leader is 5. The right most element 
+// is always a leader so it is also 
+// included.
+
+let arr =[16,17,4,3,5,2];
+
+function leader(arr) {
+  let result = Math.max(...arr);
+  return result;
+}
+
+console.log("leader",leader(arr));  
