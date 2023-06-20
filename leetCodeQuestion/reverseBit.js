ce6fd1537d9f90aceac3e380a6d39a83a52f529f
Aditya Singh <65831065+sadityaa73@users.noreply.github.com>
2023-06-20 19:38:40 +0530

    reverse 32 bit updaed

--- leetCodeQuestion/reverseBit.js
+++ leetCodeQuestion/reverseBit.js
@@ -1,20 +1,4 @@
 //WAP To Reverse Bit ;
-
-let Bit = "00000010100101000001111010011100";
-
-function binaryToDecimal(n)
-{    
-    let result=0
-    let reverse = n;
-    for(let i=reverse.length-1;i>=0;i--)
-    {
-        result += Number(reverse[i]) * Math.pow(2,i);
-        
-    }
-    console.log("result",result);
-    return result;
-}
-
 function decimalToBinary(n)
 {
     let decimalVal= n;
@@ -26,10 +10,35 @@ function decimalToBinary(n)
         result = decimalVal/divider;
         Ans.push(decimalVal%divider);
         decimalVal =Math.trunc(result);
-        console.log(decimalVal);
+        console.log("decimal",decimalVal);
     }
-    Ans.reverse();
+    if(Ans.length < 32)
+    {
+        for(let i=0;i<=32;i++)
+        {
+            if(i > Ans.length)
+            {
+                Ans.push(0);
+            }
+        }
+        Ans.reverse();
+    }
+    else{
+        Ans.reverse();
+    }
     return Ans;
 }
-
-console.log(decimalToBinary(binaryToDecimal(Bit)));
+function binaryToDecimal(array)
+{    
+    let result=0
+    let reverse = array;
+    console.log("printng reverse array",reverse);
+    for(let i=reverse.length-1;i>=0;i--)
+    {
+        result += Number(reverse[i]) * Math.pow(2,i);
+        
+    }
+    console.log("result",result);
+    return result;
+}
+console.log("print binary to decimal",binaryToDecimal(decimalToBinary(43261596)))
