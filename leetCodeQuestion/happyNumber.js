56263f60d054ba340fa53bbe605d8841d69305fb
Aditya Singh <65831065+sadityaa73@users.noreply.github.com>
2023-06-21 22:02:56 +0530

    happy number added

---
+++ leetCodeQuestion/happyNumber.js
@@ -0,0 +1,22 @@
+// Write an algorithm to determine if a n n is happy.
+
+// A happy n is a n defined by the following process:
+
+// Starting with any positive integer, replace the n by the sum of the squares of its digits.
+// Repeat the process until the n equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+// Those ns for which this process ends in 1 are happy.
+// Return true if n is a happy n, and false if not.
+
+//Example:
+// Input: n = 19
+// Output: true
+// Explanation:
+// 12 + 92 = 82
+// 82 + 22 = 68
+// 62 + 82 = 100
+// 12 + 02 + 02 = 1
+
+let input =19;
+
+
+
