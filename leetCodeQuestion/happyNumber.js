64fafee8b1ba1a154f33011e4b2fa77495b535cb
Aditya Singh <65831065+sadityaa73@users.noreply.github.com>
2023-06-22 12:17:54 +0530

    updated

--- leetCodeQuestion/happyNumber.js
+++ leetCodeQuestion/happyNumber.js
@@ -1,22 +1,18 @@
-// Write an algorithm to determine if a n n is happy.
+function isHappy(n)
+{
+  let number = n.toString().split("");
+  let result = 0;
+  console.log("number",number);
+  for(let i = 0;i<number.length;i++)
+  {
+    result += Math.pow(number[i],2);
+    console.log("result",result);
+  }
+  if(result !==1)
+  {
+    isHappy(result);
+  }
+  return true;
+}
 
-// A happy n is a n defined by the following process:
-
-// Starting with any positive integer, replace the n by the sum of the squares of its digits.
-// Repeat the process until the n equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
-// Those ns for which this process ends in 1 are happy.
-// Return true if n is a happy n, and false if not.
-
-//Example:
-// Input: n = 19
-// Output: true
-// Explanation:
-// 12 + 92 = 82
-// 82 + 22 = 68
-// 62 + 82 = 100
-// 12 + 02 + 02 = 1
-
-let input =19;
-
-
-
+console.log("is happy",isHappy(19));
